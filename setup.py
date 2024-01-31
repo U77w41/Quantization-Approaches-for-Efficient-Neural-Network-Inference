fe4c647188fe327761236f5610428a1c3f5b540a
ujjwal <ujjwal@vistaintelligence.ai>
2024-01-31 12:30:00 +0530

    added

--- setup.py
+++ setup.py
@@ -1,50 +1,59 @@
-# Run this py file before to download the requirement files
-# import subprocess
+# # Run this py file before to download the requirement files
+import subprocess
 
-# def install_libraries(requirements_file):
-#     with open(requirements_file, 'r') as file:
-#         libraries = [line.strip() for line in file.readlines() if line.strip()]
+def install_libraries(requirements_file):
+    with open(requirements_file, 'r') as file:
+        libraries = [line.strip() for line in file.readlines() if line.strip()]
 
-#     for library in libraries:
-#         subprocess.call(['pip', 'install', library])
+    for library in libraries:
+        subprocess.call(['pip', 'install', library])
 
-# if __name__ == "__main__":
-#     # Installing the requirements
-#     requirements_file = "requirements.txt"
-#     install_libraries(requirements_file)
+if __name__ == "__main__":
+    # Installing the requirements
+    requirements_file = "requirements.txt"
+    install_libraries(requirements_file)
 
-import os
 
-# Directories needed
-dirs = [
-    os.path.join("data", "raw"),
-    os.path.join("data","processed"),
-    "docs",
-    "references",
-    "reports",
-    "notebooks",
-    "saved_models",
-    "src"
-]
 
-# Create the directories and write a .gitkeep file inside each directory (we cannot commit an empty directory in git)
-for dir in dirs:
-    os.makedirs(dir , exist_ok=True)
+###########################################################################
+#      Comment out the below to create the basic project structure        #
+###########################################################################
+
+
+# import os
+
+# # Directories needed
+# dirs = [
+#     os.path.join("data", "raw"),
+#     os.path.join("data","processed"),
+#     "docs",
+#     "references",
+#     "reports",
+#     "notebooks",
+#     "saved_models",
+#     "src"
+# ]
+
+# # Creating the directories
+# for dir in dirs:
+#     if not os.listdir(dir):
+#         os.makedirs(dir , exist_ok=True)
         
 
-files = ['params.yaml',
-        '.gitignore', # Here we push all the files which we dont want to push into git
-        'requirements.txt',
-        os.path.join('src' , '__init__.py' ),
-        os.path.join('notebooks', 'quantization_tutorial.ipynb')]
+# files = ['params.yaml',
+#         '.gitignore', # Here we push all the files which we dont want to push into git
+#         'requirements.txt',
+#         os.path.join('src' , '__init__.py' ),
+#         os.path.join('notebooks', 'quantization_tutorial.ipynb')]
 
-# Creating the params and gitignore files
-for file_ in files:
-    with open(file_, "w") as f:
-        pass
+# # Creating some inistial files
+# for file_ in files:
+#     if not os.path.exists(file_):
+#         with open(file_, "w") as f:
+#             pass
     
-# Adding the .gitkeep files to the empty directories
-for dir in dirs:
-    if not os.listdir(dir):
-        with open(os.path.join(dir, '.gitkeep'), 'w') as f:
-            pass
+# # Adding the .gitkeep files to the empty directories
+# for dir in dirs:
+#     if not os.listdir(dir):
+#         with open(os.path.join(dir, '.gitkeep'), 'w') as f:
+#             pass
